9df504fab7bedb828d1666b9dcea072fa4d2b70a
Jeff Braun <jeffbr@Mac-Studio-von-Jeff.local>
2022-11-29 10:39:20 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const [formData, setFormData] = useState({});
+
+  function sendData() {
+    fetch("http://localhost:4000/orders", {
+      method: "POST",
+      body: JSON.stringify(formData),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      } 
+    })
+  }
+
+  function inputChange(e) {
+    const newFormData = {...formData}
+    newFormData[e.target.name] = e.target.value
+    setFormData(newFormData);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className='form'>
+        <input type="text" name="userId" placeholder="userId" onChange={inputChange} value={formData.userId}/>
+        <input type="text" name="recordId" placeholder="recordId" min="4" required onChange={inputChange} value={formData.recordId}/>
+        <input type="text" name="quantity" placeholder="quantity" onChange={inputChange} value={formData.quantity}/>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
